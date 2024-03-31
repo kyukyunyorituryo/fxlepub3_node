d7d9047408900fd54cf125d0bf1d67fa9c9844c6
急急如律令 <kyukyunyorituryo@live.jp>
2024-03-31 14:13:03 +0900

    データ渡し方の変更

--- example.js
+++ example.js
@@ -1,20 +1,18 @@
 var epub = require('./fxlepub3.js'); 
 
 var data={
-//"url":"C:/Users/user/Downloads/comic/test/",
-"url":"C:/Users/user/Downloads/comic/testdata/",
-"output":'C:/Users/user/Downloads/comic/fxlsample/out.epub',
+//"url":"C:\Users\Owner\Downloads\comic\testdata",
+"url":"C:/Users/Owner/Downloads/comic/testdata/",
+"output":'C:/Users/Owner/Downloads/comic/out.epub',
 "title": "タイトル",
 "author1": "著者名1",
 "author2": "著者名２",
 "index": [
     ["cover.jpg", "表紙"],
-    ["00001.jpg", "目次"],
-    ["00005.jpg", "最終ページ"]
+    ["1.jpg", "目次"],
+    ["5.jpg", "最終ページ"]
     ],
 "page_direction": "rtl",
 "panel_view": "horizontal-rl",
-"cover_file": {},
-"files": []
 }
 epub.gen(data)
